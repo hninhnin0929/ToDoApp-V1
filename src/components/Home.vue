2769cb61fe84ac6914ab8e4e930118b33ba7ba44
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-15 10:27:19 +0900

    add bootstrap border table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,31 @@
 
     <h2 class="text-center mt-5">{{ msg }}</h2>
 
-      <!-- Input -->
+    <!-- Input -->
     <div class="d-flex">
       <input type="text" class="form-control" placeholder="Enter task">
       <button class="btn btn-warning rounded-0">SUBMIT</button>
     </div>
+
+    <!-- Task Table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Read Vuejs2.0 documentation</th>
+          <td>To-do</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
 
   </div>
 </template>
@@ -15,7 +35,7 @@
 <script>
 export default {
   name: 'Home',
-  data () {
+  data() {
     return {
       msg: 'Welcome to My Todo App'
     }
@@ -25,7 +45,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 </style>
